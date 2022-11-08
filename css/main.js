bf3fbb2ae75d695c5aa99711e202fcd5254b244c
rgmanikandan07 <rgmanimani0707@gmail.com>
2022-11-08 21:04:02 +0530

    add new file

--- css/main.js
+++ css/main.js
@@ -1,5 +1,29 @@
-let num1 = 22;
+// Show Menu
 
-let num2 = 23;
+const showMenu = (toggleId,navId) => {
+    const toggle = document.getElementById(toggleId),
+          nav = document.getElementById(navId)
 
-console.log(num1+=num2);
+          if(toggle && nav){
+            toggle.addEventListener('click',() => {
+                nav.classList.toggle('show')
+            });
+          }
+}
+showMenu('nav-toggle','nav-menu')
+
+// Active & Remove Active
+
+const navLink = document.querySelectorAll('.nav-link')
+navLink.forEach(n => n.classList.remove('active'))
+
+function linkAction (){
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active') 
+
+    // sidebar Hidden
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click',linkAction))  
